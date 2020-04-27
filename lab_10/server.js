c141ab416f519510db696f9131d9d9a73e30da24
eddiekoski <ekostreski@gmail.com>
2020-04-26 22:09:57 -0400

    Lab 10 Step 5
    I did not understand the professor supplied code until after I did things from scratch :'((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((
    
    On successfully saving that data, use res.json to send a JSON-encoded success message back to your index.html file.
    
        It must be different than those shown in class.

--- lab_10/server.js
+++ lab_10/server.js
@@ -50,63 +50,52 @@ app
     res.send("Get Request");
   })
   .post((req, res) => {
-    const req_name = req.body.name;
-    const req_zipcode = req.body.zipcode;
-    const req_interest = req.body.interests;
-    console.log("\n");
-    console.log("req_name",req_name);
-    console.log("\n");
-    console.log("req_zipcode",req_zipcode);
-    console.log("\n");
-    console.log("req_interest",req_interest);
-    console.log("\n");
-    // processDataForFrontEnd(req, res)
-    (async () => {
-      const db = await open(dbSettings);
-      //https://www.w3schools.com/php/php_mysql_create_table.asp
-      //auto increment is implicit THANK YOU : https://www.sqlitetutorial.net/sqlite-autoincrement/
-      const create_command = `CREATE TABLE IF NOT EXISTS SUBMISSIONS (
-        name TEXT NOT NULL,
-        zipcode TEXT NOT NULL,
-        interest TEXT NOT NULL) `;
-      console.log(create_command);
-      const result = await db.all(create_command);
-      console.log("Create Database:", result);
-
-      const insert_command = `INSERT INTO SUBMISSIONS (name, zipcode, interest)
-      VALUES('${req_name}', '${req_zipcode}', '${req_interest}') `;
-      console.log(insert_command);
-      const result2 = await db.all(insert_command);
-      console.log("Insert Submission:", result2);
-
-      const select_command = "SELECT * FROM SUBMISSIONS";
-      console.log(select_command);
-
-      const result3 = await db.all(select_command);
-      //const result = await db.all("SELECT * FROM user");
-
-      console.log("Current Database:", result3);
-
-      res.json(result3);
-    })();
-
-    console.log("POST POST POST");
-    console.log("/api post request", req.body);
     if (!req.body.name) {
       console.log(req.body);
       res
         .status("418")
         .send("something went wrong, additionally i am a teapot");
     } else {
-      writeUser(req.body.name, dbSettings)
-        .then((result) => {
+      const req_name = req.body.name;
+      const req_zipcode = req.body.zipcode;
+      const req_interest = req.body.interests;
+      console.log("\n");
+      console.log("req_name", req_name);
+      console.log("\n");
+      console.log("req_zipcode", req_zipcode);
+      console.log("\n");
+      console.log("req_interest", req_interest);
+      console.log("\n");
+      // processDataForFrontEnd(req, res)
+      (async () => {
+        const db = await open(dbSettings);
+        //https://www.w3schools.com/php/php_mysql_create_table.asp
+        //auto increment is implicit THANK YOU : https://www.sqlitetutorial.net/sqlite-autoincrement/
+        const create_command = `CREATE TABLE IF NOT EXISTS SUBMISSIONS (
+        name TEXT NOT NULL,
+        zipcode TEXT NOT NULL,
+        interest TEXT NOT NULL) `;
+        console.log(create_command);
+        const result = await db.all(create_command);
+        console.log("Create Database:", result);
 
-          console.log(result);
-          res.send("your request was successful"); // simple mode
-        })
-        .catch((err) => {
-          console.log(err);
-        });
+        const insert_command = `INSERT INTO SUBMISSIONS (name, zipcode, interest)
+      VALUES('${req_name}', '${req_zipcode}', '${req_interest}') `;
+        console.log(insert_command);
+        const result2 = await db.all(insert_command);
+        console.log("Insert Submission:", result2);
+
+        const select_command = "SELECT * FROM SUBMISSIONS";
+        console.log(select_command);
+
+        const result3 = await db.all(select_command);
+        //const result = await db.all("SELECT * FROM user");
+
+        console.log("Current Database:", result3);
+
+        res.json(result3);
+        
+      })();
     }
   })
   .put((req, res) => {
@@ -121,3 +110,27 @@ app
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
 });
+
+/*****
+ * OH NO I DID IT THE HARDWAY 
+ * 
+ *       
+console.log("POST POST POST");
+      console.log("/api post request", req.body);
+      if (!req.body.name) {
+        console.log(req.body);
+        res
+          .status("418")
+          .send("something went wrong, additionally i am a teapot");
+      } else {
+        writeUser(req.body.name, dbSettings)
+          .then((result) => {
+            console.log(result);
+            res.send("your request was successful"); // simple mode
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }
+ * 
+ */
